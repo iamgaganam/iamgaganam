ce66b1f48de0d65d899f5edbdb9614efb3e5d03e
Gagana Methmal (GM) <gaganam220@gmail.com>
2025-05-31 01:42:55 +0530

    Project Structure

---
+++ portfolio/src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Home from "./Home"; // Add this import line
+
+function App() {
+  return (
+    <div className="App">
+      <Home /> {/* Add your Home component here */}
+    </div>
+  );
+}
+
+export default App;
